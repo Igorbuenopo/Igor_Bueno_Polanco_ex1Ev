8629d3c4beb68cd6450952ba243ee264aff6a007
Igorbuenopo <igor.buenopo@elorrieta-errekamari.com>
2023-11-10 11:42:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/igor_bueno_polanco_ex1ev/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.igor_bueno_polanco_ex1ev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Igor_Bueno_Polanco_ex1Ev"
+include(":app")
+ 
